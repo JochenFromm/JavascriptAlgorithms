c2d72883c69e47f59c7ac9d03b361b16c8ee8cd8
Jochen Fromm <j.fromm@reply.de>
2018-02-17 18:50:29 +0100

    Add filter
    by using lodash

--- algorithms/map_reduce.js
+++ algorithms/map_reduce.js
@@ -1,17 +1,22 @@
+const _ = require('lodash');
 
-function letterFrequency(text) {
-  return countItems(text, 'letters');
+function letterFrequency(text, min = 3) {
+  const results = countItems(text, 'letters');
+  return _.pickBy(results, function (value, key) { return value > min } );
 }
 
-function wordFrequency(text) {
-  return countItems(text, 'words');
+function wordFrequency(text, min = 3) {
+  let results = countItems(text, 'words');
+  return _.pickBy(results, function (value, key) { return value > min } );
 }
 
 function countItems(text, item) {
   separator = (item == 'letters') ? '' : ' '
 
   // map phase
-  words = text.split(separator).map(function(word){ return word.replace(/[\.,]/g,'') });
+  words = text.split(separator).map(function(word){
+    return word.toLowerCase().replace(/[\.,]/g,'')
+  });
 
   // reduce phase
   hash = words.reduce(function(x, y) {
